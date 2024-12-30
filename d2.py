cd63c51c95d7dc8eef22594bb6c70f935f042940
EllipticBike38 <andrea.mazzini00@gmail.com>
2024-12-30 15:57:14 +0100

    Enhance check_safety function to allow for a second chance at validation

--- d2.py
+++ d2.py
@@ -10,13 +10,25 @@ def read_from_file(file_path: str) -> str:
     with open(file_path, 'r') as file:
         return file.read()
     
-def check_safety(report:str) -> bool:
+def check_safety(report:str, second_chance = True) -> bool:
     report = report.split()
     increase = 1 if int(report[0]) < int(report[1]) else -1
     DEBUG and print('increase' if increase == 1 else 'decrease')
     if increase == -1:
         report = report[::-1]
-    return all([ 0<int(report[i+1]) - int(report[i])<4 for i in range(len(report)-1)])
+    for i in range(0, len(report)):
+        if i == len(report) - 1:
+            break
+        DEBUG and print(f'{report[i]} - {report[i+1]} = {int(report[i+1]) - int(report[i])}')
+        if int(report[i+1]) - int(report[i])>3 or int(report[i+1]) - int(report[i])<1:
+            DEBUG and print('Not safe, trying to fix')
+            if second_chance:
+                # provo a togliere il valore rotto, quello prima o quelo dopo
+                return any([check_safety(' '.join(report[:j] + report[j+1:]), False) for j in range(len(report))])
+            return False
+            
+            
+    return True
 
 def main(input_string: str) -> str:
     DEBUG and print(input_string)
